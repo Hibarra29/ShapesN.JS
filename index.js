4d9c4d3086d030e0ea36652e3b3f290b2a5cfb1f
Mona Lisa <128428403+Hibarra29@users.noreply.github.com>
2023-06-26 16:09:56 -0500

    Added code to index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-//Import Inquirer 
+//Import Inquirer and file systme
 const inquirer = require('inquirer');
-// Immport File System 
 const fs = require('fs');
+
+// import classes from shape.js
+const {Triangle, Square, Circle } = require('./lib/shape');
+
+//function that writes Svg file using answers from the prompts
+function writeTOFIle(fileName, answers) {
+    let svgString = '';
+    //width and height of svg
+    svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    //keeps text in proper place using g tags 
+    svgString += '<g>';
+    //takes shape input
+    svgString += '${answers.shape}';
+}
