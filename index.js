af6a01871c26c767cc56da0ed9f2386d7549ead5
Mona Lisa <128428403+Hibarra29@users.noreply.github.com>
2023-06-27 14:58:15 -0500

    fixed issue with code not creating proper svg file

--- index.js
+++ index.js
@@ -1,79 +1,83 @@
-//Import Inquirer and file systme
-const inquirer = require('inquirer');
+// Required packages for project 
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const fs = require('fs');
+// Leads path to required files for shape modeling
+const { Triangle, Square, Circle } = require("./lib/shape");
 
-// import classes from shape.js
-const {Triangle, Square, Circle } = require('./lib/shape');
-
-//function that writes Svg file using answers from the prompts
-function writeTOFIle(fileName, answers) {
-    let svgString = '';
-    //width and height of svg
-    svgString =
+// Function that takes user input from prompts to provide shape information 
+function writeToFile(fileName, answers) {
+  
+  let svgString = "";
+  svgString =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-    //keeps text in proper place using g tags 
-    svgString += '<g>';
-    //takes shape input
-    svgString += '${answers.shape}';
+  
+  svgString += "<g>";
+  svgString += `${answers.shape}`;
 
-    // Takes user input and adds polygon properties and colors 
-    let shapeChoice;
-    if (answers.shape === 'Triangle') {
-        shapeChoice = new Triangle();
-        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
-    } else if (answers.shape === "Square") {
-        shapeChoice = new Square();
-        svgString += `<rec x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;   
-    } else {
-        shapeChoice = new Circle();
-        svgString =+ `<circle cx="150"cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
-    }
-    //text tag allows for uniform text format 
-    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40"fill="${answers.textColor}">${answers.text}</text>`;
-    svgString += "</g>";
-    svgString += "</svg>";
+  // Takes user input from prompt to add shape style to svg file
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
 
-    //using fs to make svg 
-    fs.writeFile(fileName,svgString, (err)=> {
-        err ? console.log(err) : console.log("Created logo.svg");
-    });
+  // Use of text tag to align text 
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+  svgString += "</g>";
+  svgString += "</svg>";
+
+  //Use of fs to create file 
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
 }
 
-//Using Inquirer to send questions on command line using prompts
+//Function to generate the answers using prompts for user input
 function promptUser() {
-    inquirer 
+  inquirer
     .prompt([
-        {
-            type:"input",
-            message:"Enter Text to display. (Up to three Characters)",
-            name: "text",
-        },
-        {
-            type:"input",
-            message: "Enter text color or hexdecimal number.",
-            name: "textColor",
-        },
-        {
-            type:"list",
-            message: "Select a shape to display.",
-            choices: ["Circle", "Triangle", "Square"],
-            name:"shape",
-        },
-        {
-            type:"input",
-            message: "Enter a shape color or hexdecimal number.",
-            name: "shapeBackgroundColor",
-        },
+      {
+        type: "input",
+        message:
+          "Enter text to display, Up to 3 characters",
+        name: "text",
+      },
+      {
+        type: "input",
+        message:
+          "Choose text color or hex code",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "Choose a shape",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message:
+          "Enter a shape color or hex code",
+        name: "shapeBackgroundColor",
+      },
     ])
-    .then((answers)=> {
-        if (answers.text.length > 3) {
-            console.log("Must provide a value of no more than 3 characters");
-            promptUser();
-        } else {
-            writeTOFIle("logo.svg",answers);
-        }
+    //Generates logo.svg file
+    .then((answers) => {
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
     });
 }
-//Starting Function
+
+//starting function 
 promptUser();
